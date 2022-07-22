782a3cdd082e12bff16d8ceadce9f198a3880699
kdamichie <kurtis.michie@gmail.com>
2022-07-22 10:34:00 -0600

    Development and Production 'connection' set to var

--- knexfile.js
+++ knexfile.js
@@ -20,7 +20,7 @@ const URL =
 module.exports = {
   development: {
     client: "postgresql",
-    connection: DATABASE_URL,
+    connection: DEVELOPMENT_DATABASE_URL,
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
@@ -32,7 +32,7 @@ module.exports = {
 
   production: {
     client: "postgresql",
-    connection: DATABASE_URL,
+    connection: PRODUCTION_DATABASE_URL,
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
