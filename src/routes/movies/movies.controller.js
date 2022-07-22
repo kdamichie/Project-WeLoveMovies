735090ce6af914e74a1c79418b684f6cad71e0b6
kdamichie <kurtis.michie@gmail.com>
2022-07-22 13:59:59 -0600

    Created movies.controller

--- src/routes/movies/movies.controller.js
+++ src/routes/movies/movies.controller.js
@@ -2,28 +2,46 @@ const service = require("./movies.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
 async function movieExists(req, res, next) {
-
+  const { movieId } = req.params;
+  const movie = await service.read(movieId);
+  
+  if (movie) {
+    res.locals.movie = movie;
+    return next();
+  }
+  next({
+    status: 404,
+    message: `Movie cannot be found.`
+  });
 }
 
 async function list(req, res) {
-
+  const { is_showing } = req.query;
+  if(is_showing) {
+    const data = await service.listIsShowing();
+    return res.json({ data });
+  }
+  const data = await service.list();
+  res.json({ data });
 }
 
 function read(req, res) {
-
+  res.json({ data: res.locals.movie });
 }
 
-async function listTheaters(req, res) {
-
+async function listTheatersByMovie(req, res) {
+  const movieId = await res.locals.movie.movie_id;
+  res.json({ data: service.listTheatersByMovie(movieId) });
 }
 
-async function listReviews(req, res) {
-
+async function listReviewsByMovie(req, res) {
+  const movieId = await res.locals.movie.movie_id;
+  res.json({ data: service.listReviewsByMovie(movieId) });
 }
 
 module.exports = {
   list: asyncErrorBoundary(list),
   read: [asyncErrorBoundary(read), read],
-  listTheaters: [asyncErrorBoundary(movieExists), asyncErrorBoundary(listTheaters)],
-  listReviews: [asyncErrorBoundary(movieExists), asyncErrorBoundary(listReviews)],
+  listTheaters: [asyncErrorBoundary(movieExists), asyncErrorBoundary(listTheatersByMovie)],
+  listReviews: [asyncErrorBoundary(movieExists), asyncErrorBoundary(listReviewsByMovie)],
 }
