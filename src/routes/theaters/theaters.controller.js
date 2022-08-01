1b000276ac16923225e8d02c71db7e05814d5331
kdamichie <kurtis.michie@gmail.com>
2022-08-01 14:24:02 -0600

    created theaters.controller

--- src/routes/theaters/theaters.controller.js
+++ src/routes/theaters/theaters.controller.js
@@ -2,7 +2,8 @@ const service = require("./theaters.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
 async function list(req, res) {
-
+  const data = await service.list();
+  res.json({ data });
 }
 
 module.exports = {
