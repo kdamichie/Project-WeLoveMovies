42b1c9d13e27f7988e5e0c6f1efa6a3443eb2bf7
kdamichie <kurtis.michie@gmail.com>
2022-08-01 14:31:36 -0600

    created theaters.service layout

---
+++ src/routes/theaters/theaters.service.js
@@ -0,0 +1,12 @@
+const knex = require("../../db/connection");
+const reduceProperties = require("../../utils/reduce-properties");
+
+const reduceMovies = reduceProperties()
+
+function list() {
+
+}
+
+module.exports = {
+  list,
+}
