678b548111a3911c83d4dd6379c5f9f461a2b6df
kdamichie <kurtis.michie@gmail.com>
2022-08-01 14:39:06 -0600

    created theaters.service

--- src/routes/theaters/theaters.service.js
+++ src/routes/theaters/theaters.service.js
@@ -1,10 +1,20 @@
 const knex = require("../../db/connection");
 const reduceProperties = require("../../utils/reduce-properties");
 
-const reduceMovies = reduceProperties()
+const reduceMovies = reduceProperties("theater_id", {
+  movie_id: ["movies", null, "movie_id"],
+  title: ["movies", null, "title"],
+  runtime_in_minutes: ["movies", null, "runtime_in_minutes"],
+  rating: ["movies", null, "rating"],
+  description: ["movies", null, "description"],
+  image_url: ["movies", null, "image_url"],
+}); 
 
 function list() {
-
+  return knex("theaters as t")
+    .join("movies_theaters as mt", "mt.thater_id", "t.theater_id")
+    .join("movies as m", "m.movie_id", "mt.movie_id")
+    .then(reduceMovies);
 }
 
 module.exports = {
