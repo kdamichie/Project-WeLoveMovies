e72736bf0b5e9e9b3a3439b9dd6f90e223c8d96e
kdamichie <kurtis.michie@gmail.com>
2022-08-05 11:03:46 -0600

    refactored read and mapProperties function in reviews.service

--- src/routes/reviews/reviews.service.js
+++ src/routes/reviews/reviews.service.js
@@ -2,12 +2,12 @@ const knex = require("../../db/connection");
 const mapProperties = require("../../utils/map-properties");
 
 const mapCriticProperties = mapProperties({
-  critic_id: "critic.critic_id",
-  preferred_name: "critic.preffered_name",
+  critic_critic_id: "critic.critic_id",
+  preferred_name: "critic.preferred_name",
   surname: "critic.surname",
   organization_name: "critic.organization_name",
-  created_at: "critic.created_at",
-  updated_at:"critic.updated_at",
+  critic_created_at: "critic.created_at",
+  critic_updated_at: "critic.updated_at",
 });
 
 function update(updatedReview) {
@@ -18,11 +18,21 @@ function update(updatedReview) {
     .then((updatedReview) => updatedReview[0]);
 }
 
-function read(reviewId) {
-  return knex("reviews")
-  .select("*")
-  .where({ review_id : reviewId })
-  .first();
+function read(review_id) {
+  return knex("reviews as r")
+    .join("critics as c", "c.critic_id", "r.critic_id")
+    .select(
+      "r.*",
+      "c.critic_id as critic_id",
+      "c.preferred_name",
+      "c.surname",
+      "c.organization_name",
+      "c.created_at as created_at",
+      "c.updated_at as updated_at"
+    )
+    .where({ review_id })
+    .first()
+    .then(mapCriticProperties);
 }
 
 function destroy(reviewId) {
