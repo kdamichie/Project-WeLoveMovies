3139f62014ad5af45008fc18d703e260d8f9f5ce
kdamichie <kurtis.michie@gmail.com>
2022-08-05 10:48:24 -0600

    Added JSON parsing and CORS into app.js.  PUT method was failing

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ const moviesRouter = require("./routes/movies/movies.router");
 const reviewsRouter = require("./routes/reviews/reviews.router");
 const theatersRouter = require("./routes/theaters/theaters.router");
 
+app.use(cors());
+app.use(express.json());
+
 app.use("/movies", moviesRouter);
 app.use("/reviews", reviewsRouter);
 app.use("/theaters", theatersRouter);
